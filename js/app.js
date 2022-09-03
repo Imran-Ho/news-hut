684b238d127203d7cfdb634c6eba316afdd2968e
Imran-Ho <imranhossain36@gmail.com>
2022-09-03 09:58:56 +0600

    categories

--- js/app.js
+++ js/app.js
@@ -1,7 +1,21 @@
 const loadNewsCategories = () =>{
     fetch('https://openapi.programming-hero.com/api/news/categories')
     .then(res => res.json())
-    .then(data => console.log(data.data.news_category) )
+    .then(data => displayNewsCategory(data.data.news_category) )
+}
+
+const displayNewsCategory =(categories)=>{
+    const newsContainerId = document.getElementById('news-categories');
+    categories.forEach(category => {
+        console.log(category)
+        const newsDiv = document.createElement('div');
+        newsDiv.classList.add('category');
+        newsDiv.innerHTML = `
+            <div>${category.category_name}</div>
+        `;
+        newsContainerId.appendChild(newsDiv);
+
+    })
 }
 
 loadNewsCategories()
